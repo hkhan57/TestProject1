b2cc3f30b86bf6c1c39c40ba3945612f7d53f8d2
Hammad Khan <madakhan@gmail.com>
2017-07-30 16:49:44 -0400

    My genius program

--- test.cpp
+++ test.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-int main(void)
+int main(int argc, char* argv[])
 {
-   cout << "Hello, world!\n";
-   
+   if (argc != 2) {
+      cerr << "Please provide a string arg\n";
+      return 1;
+   }
+
+   const char* test = argv[1];
+
+   for (const char* p_stop = test; *p_stop; p_stop++)
+   {
+      for (const char* p = test; p <= p_stop; p++)
+      {
+         cout << *p;
+      }
+      cout << '\n';
+   }
+
    return 0;
 }
